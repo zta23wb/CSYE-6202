ed1430ce45a75a85ac99c1d22c47ba214592ac8c
baristestuser <byanmaz@gmail.com>
2015-09-20 09:57:40 -0400

    Prime Numbers skeleton files

---
+++ FindingPrimeNumbers/FindingPrimeNumbers.cs
@@ -0,0 +1,14 @@
+﻿namespace FindingPrimeNumbers
+{
+	public class FindingPrimeNumbers
+	{
+		public long FindSumOfPrimeNumbers(int n)
+		{
+			long sum = 0;
+
+			// your solution goes in here
+
+			return sum;
+		}
+	}
+}
---
+++ FindingPrimeNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FindingPrimeNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var findingPrimeNumbers = new FindingPrimeNumbers();
+
+			// no results
+			//findingPrimeNumbers.FindPrime(0);
+
+			// 2
+			//findingPrimeNumbers.FindPrime(1);
+
+			// 5
+			//2, 3
+			//findingPrimeNumbers.FindPrime(2);
+
+			// 28
+			// 2, 3, 5, 7, 11
+			//findingPrimeNumbers.FindPrime(5);
+
+			// 129
+			// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+			//findingPrimeNumbers.FindPrime(10);
+
+			// 1060
+			// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+			//findingPrimeNumbers.FindPrime(25);
+		}
+	}
+}
+																																																		
