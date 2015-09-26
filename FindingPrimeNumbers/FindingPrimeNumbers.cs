328e03c6d01544d7376c71c320eb0a017f22c78c
Zach <tang.zi@husky.neu.edu>
2015-09-26 17:41:35 -0400

    9/26

--- FindingPrimeNumbers/FindingPrimeNumbers.cs
+++ FindingPrimeNumbers/FindingPrimeNumbers.cs
@@ -6,9 +6,35 @@
 		{
 			long sum = 0;
 
-			// your solution goes in here
-
-			return sum;
+            // your solution goes in here
+            int numberHasFound = 0;
+            int numberCheck = 2;
+            bool isPrimeNumber = true;
+            while (numberHasFound < n)
+            {
+                for (int i = 2; i <= numberCheck - 1; i++)
+                {
+                    if (numberCheck % i == 0)
+                    {
+                        isPrimeNumber = false;
+                    }
+                    if (i >= numberCheck)
+                    {
+                        isPrimeNumber = true;
+                    }
+                }
+                if (isPrimeNumber == true)
+                {
+                    numberHasFound++;
+                    sum += numberCheck;
+                }
+                else
+                {
+                    isPrimeNumber = true;
+                }
+                numberCheck++;
+            }
+            return sum;
 		}
 	}
 }
