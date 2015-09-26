01937e23e3a6a2b18d157d4c7a3eea6f5deed2b4
Zach <tang.zi@husky.neu.edu>
2015-09-26 17:17:59 -0400

    9/26

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -6,9 +6,31 @@
 		{
 			string result = number.ToString();
 
-			// your solution/implementation should go in here
+            // your solution/implementation should go in here
+            if (number == 0)
+            {
+                return result;
+            }
+            switch (number % 15)
+            {
+                case 0:
+                    result = "FizzBuzz";
+                    break;
+                case 3:
+                case 6:
+                case 9:
+                case 12:
+                    result = "Fizz";
+                    break;
+                case 5:
+                case 10:
+                    result = "Buzz";
+                    break;
+                default:
+                    break;
+            }
 
-			return result;
+            return result;
 		}
 	}
 }
