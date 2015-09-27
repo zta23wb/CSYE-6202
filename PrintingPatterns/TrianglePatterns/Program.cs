28eca37a39b699d241614975d5ae932852c3f465
Baris Yanmaz <byanmaz@ezesoft.com>
2015-09-27 13:53:43 -0400

    Checked in the Patterns assignment

---
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TrianglePatterns
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DisplayPatternA();
+			DisplayPatternB();
+			DisplayPatternC();
+			DisplayPatternD();
+
+			Console.ReadLine();
+		}
+
+		static void DisplayPatternA()
+		{
+			// your implementation here
+		}
+
+		static void DisplayPatternB()
+		{
+			// your implementation here
+		}
+
+		static void DisplayPatternC()
+		{
+			// your implementation here
+		}
+
+		static void DisplayPatternD()
+		{
+			// your implementation here
+		}
+	}
+}
