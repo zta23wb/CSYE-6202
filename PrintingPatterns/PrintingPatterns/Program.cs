11c2a04dd5bf6055ab7ce43c4b82e92e41645967
Zach <tang.zi@husky.neu.edu>
2015-09-30 15:43:14 -0400

    9/30

---
+++ PrintingPatterns/PrintingPatterns/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintPattern(10, 1, 1, "*", " ");
+            Console.WriteLine();
+            PrintPattern(10, 10, -1, "*", " ");
+            Console.WriteLine();
+            PrintPattern(10, 0, 1, " ", "*");
+            Console.WriteLine();
+            PrintPattern(10, 9, -1, " ", "*");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        public static void PrintPattern(int lineNum, int startPatternOne, int goingDown, String patternOne, String patternTwo) {
+            for(int line = 1; line <= lineNum; line++)
+            {
+                for(int i = 1; i <= startPatternOne; i++)
+                {
+                    Console.Write(patternOne);
+                }
+                for (int j = 0; j < lineNum - startPatternOne; j++)
+                {
+                    Console.Write(patternTwo);
+                }
+                startPatternOne += goingDown;
+                Console.WriteLine();
+            }
+        }
+    }
+}
