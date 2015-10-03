9cd884b97de5b7185078c63b4e61936e558ecaf1
Zach <tang.zi@husky.neu.edu>
2015-10-03 18:20:56 -0400

    Comitting PA

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -6,32 +6,32 @@ namespace TrianglePatterns
 	{
 		static void Main(string[] args)
 		{
-			DisplayPatternA();
-			DisplayPatternB();
-			DisplayPatternC();
-			DisplayPatternD();
+            PrintPattern(10, 1, 1, "*", " ");
+            Console.WriteLine();
+            PrintPattern(10, 10, -1, "*", " ");
+            Console.WriteLine();
+            PrintPattern(10, 0, 1, " ", "*");
+            Console.WriteLine();
+            PrintPattern(10, 9, -1, " ", "*");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
 
-			Console.ReadLine();
-		}
-
-		static void DisplayPatternA()
-		{
-			// your implementation here
-		}
-
-		static void DisplayPatternB()
-		{
-			// your implementation here
-		}
-
-		static void DisplayPatternC()
-		{
-			// your implementation here
-		}
-
-		static void DisplayPatternD()
-		{
-			// your implementation here
-		}
-	}
+        public static void PrintPattern(int lineNum, int startPatternOne, int goingDown, String patternOne, String patternTwo)
+        {
+            for (int line = 1; line <= lineNum; line++)
+            {
+                for (int i = 1; i <= startPatternOne; i++)
+                {
+                    Console.Write(patternOne);
+                }
+                for (int j = 0; j < lineNum - startPatternOne; j++)
+                {
+                    Console.Write(patternTwo);
+                }
+                startPatternOne += goingDown;
+                Console.WriteLine();
+            }
+        }
+    }
 }
